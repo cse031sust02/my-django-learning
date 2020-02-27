0ab5a3b55ff4f7b687d06f8ebd12655b7f652d77
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2020-02-27 17:22:09 +0600

    add model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    pub_date = models.DateField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
