3790ffa4fb494112181615c82ae17014feda2c29
cse031sust02 <cse031sust02.freelancer@gmail.com>
2020-10-19 15:06:41 +0600

    update db settings to PostgreSQL

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = 'my_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'play_django',
+        'USER': 'postgres',
+        'PASSWORD': 'docker',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    },
 }
 
 
