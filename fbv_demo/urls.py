2322a50333de1da086364bd908e3351dae08e9f1
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2021-03-07 15:10:15 +0600

    added templates in function based view demo

--- fbv_demo/urls.py
+++ fbv_demo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('articles/', views.artcile_list, name='article-list')
+    path('articles/', views.article_list, name='article-list'),
+    path('articles/<int:article_id>/', views.article_details, name='article-detail'),
 ]
--- fbv_demo/views.py
+++ fbv_demo/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 
 from blog.models import Article
 from blog.serializers import ArticleSerializer
 
 
-def artcile_list(request):
-    artcile_list = Article.objects.order_by('-pub_date')
-    output = ', '.join([q.title for q in artcile_list])
-    return HttpResponse(output)
+def article_list(request):
+    article_list = Article.objects.order_by('-pub_date')
+    context = {'article_list': article_list}
+    return render(request, 'articles/index.html', context)
+
+
+def article_details(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'articles/detail.html', {'article': article})
