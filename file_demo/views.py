ccac91b65f1b7b0cf09d07a46176b058f70fdddf
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2021-07-05 10:07:39 +0600

    play with files

--- file_demo/views.py
+++ file_demo/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
+from .forms import UploadFileForm
+
 
 def file_upload_demo(request):
-    context = {}
-    return render(request, 'file_demo/index.html', context)
+    if request.method == 'GET':
+        context = {}
+        return render(request, 'file_demo/index.html', context)
+    elif request.method == 'POST':
+        # form = UploadFileForm(request.POST, request.FILES)
+        # if form.is_valid():
+        #     print("VALID");
+        #     print(request.FILES)
+
+        context = {'request': request}
+        print(request.FILES)
+        return render(request, 'file_demo/result.html', context)
